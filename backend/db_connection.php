f6b333bb0ca69fb8084fd94f304c85016f1f540d
N4EXM <naemmmail89@gmail.com>
2025-07-17 17:54:20 +0100

    doing the backend from scratch

--- backend/db_connection.php
+++ backend/db_connection.php
@@ -1,16 +1,22 @@
-<?php
-        $servername = "127.0.0.1";
-        $username = "root";
-        $password = "";
-        $db_name = "login_system";
+<?php 
 
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-    } 
-    catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage(); 
-    }
-?> 
+$host = "localhost";
+$db = "login_system";
+$username = "root";
+$password = "";
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
+
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+    // echo "connected";
+}
+catch (PDOException $e) {
+    die("database connection failed" . $e->getMessage());
+}
