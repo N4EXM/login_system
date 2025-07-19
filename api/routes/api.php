210b4274c31a347c41e03519f117b3e0c38792ef
N4EXM <naemmmail89@gmail.com>
2025-07-19 09:27:23 +0100

    php api is not working

--- api/routes/api.php
+++ api/routes/api.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
 ob_start();
 
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
